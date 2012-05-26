91bfec4b82bd9fd200ca00ea1311e24c2f8ed2b1
Tanmay Sinha <sinha.tanmay@gmail.com>
2012-05-26 19:22:37 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Saakshar::Application.config.secret_token = '362020197d9cd95bf7deff43d634dbdd471c29befe4d96e21119b61904affa79d43786892e9c1ffd1711e6e48c817c5f254b7c7d44cad2da3e932162b2bc29c7'
