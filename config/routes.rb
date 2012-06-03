d730ca379277e6b8660cba08239ae73b111af464
Tanmay Sinha <sinha.tanmay@gmail.com>
2012-06-03 12:26:10 -0400

    Handle callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Saakshar::Application.routes.draw do
   post '/login' => 'sessions#create', :as => :login
   get '/logout' => 'sessions#destroy', :as => :logout
+  match '/auth/:provider/callback', :to => 'sessions#create'
+  
   root :to => 'home#index'
 end
